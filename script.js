7d420e59133e6997ff1903bc497af8cc26f47a01
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-10-08 20:52:00 +0300

    now populating dynamic on screen cart with array data

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const itm_element = document.querySelector("#item-select");
 const qty_element = document.querySelector("#quantity");
+const cart_element = document.querySelector("#cart");
 
 let cart_items = [];
 
 document.querySelector("#add").addEventListener("click", () => {
-  cart_items += {
+  cart_items.push({
     item: itm_element.value,
     quantity: qty_element.value,
-  };
-  console.log(cart_items)
+  });
+
+  cart_element.innerHTML = "";
+  cart_items.forEach((element) => {
+    cart_element.innerHTML += `
+    <div>
+        <div>
+            name: ${element.item}
+        </div>
+        <div>
+            quantity: ${element.quantity}
+        </div>
+    </div>
+    `;
+  });
 });
